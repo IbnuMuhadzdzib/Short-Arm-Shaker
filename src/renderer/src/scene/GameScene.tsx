ecefef0efa9c03a9fc452c37a2b6ae5ab50a3842
IbnuMuhadzdzib <ibnulaginugas@gmail.com>
2026-09-09 11:49:29 +0700

    [desktop/scene] fix: replace HDR environment preset with procedural Lightformer to avoid CSP/network fetch

--- src/renderer/src/scene/GameScene.tsx
+++ src/renderer/src/scene/GameScene.tsx
@@ -1,6 +1,6 @@
 import { Canvas } from '@react-three/fiber'
 import { Physics, RigidBody } from '@react-three/rapier'
-import { OrbitControls } from '@react-three/drei'
+import { OrbitControls, Environment, Lightformer } from '@react-three/drei'
 import { DiceGroup } from './DiceGroup'
 import type { Dice } from '@yahtzee/shared'
 
@@ -12,16 +12,42 @@ interface GameSceneProps {
 export function GameScene({ dice, isRolling }: GameSceneProps) {
   return (
     <Canvas camera={{ position: [0, 8, 6], fov: 50 }}>
-      <ambientLight intensity={0.5} />
-      <directionalLight position={[5, 10, 5]} intensity={1} castShadow />
+  <ambientLight intensity={0.5} />
+  <directionalLight position={[5, 10, 5]} intensity={1} castShadow />
 
-      <Physics gravity={[0, -20, 0]}>
-        <DiceGroup dice={dice} isRolling={isRolling} />
-        <Table />
-      </Physics>
+  <Environment resolution={256}>
+    <Lightformer
+      form="rect"
+      intensity={2}
+      position={[0, 5, -5]}
+      scale={[10, 10, 1]}
+      color="white"
+    />
+    <Lightformer
+      form="rect"
+      intensity={1}
+      position={[-5, 3, 5]}
+      scale={[5, 5, 1]}
+      rotation={[0, Math.PI / 2, 0]}
+      color="#aabbff"
+    />
+    <Lightformer
+      form="rect"
+      intensity={1}
+      position={[5, 3, 5]}
+      scale={[5, 5, 1]}
+      rotation={[0, -Math.PI / 2, 0]}
+      color="#ffddaa"
+    />
+  </Environment>
 
-      <OrbitControls enablePan={false} maxPolarAngle={Math.PI / 2.2} />
-    </Canvas>
+  <Physics gravity={[0, -20, 0]}>
+    <DiceGroup dice={dice} isRolling={isRolling} />
+    <Table />
+  </Physics>
+
+  <OrbitControls enablePan={false} maxPolarAngle={Math.PI / 2.2} />
+</Canvas>
   )
 }
 
